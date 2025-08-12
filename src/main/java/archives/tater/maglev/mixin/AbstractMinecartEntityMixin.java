05da2d79c13ddd84df6bde43cad97b2ce510d2cd
JosiahFu <josiahfu@gmail.com>
2025-08-11 22:36:28 -0700

    Fix switching heights and crash

--- src/main/java/archives/tater/maglev/mixin/AbstractMinecartEntityMixin.java
+++ src/main/java/archives/tater/maglev/mixin/AbstractMinecartEntityMixin.java
@@ -1,8 +1,9 @@
 package archives.tater.maglev.mixin;
 
 import archives.tater.maglev.init.MaglevBlocks;
-import archives.tater.maglev.init.MaglevDataAttachments;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Cancellable;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.vehicle.AbstractMinecartEntity;
@@ -10,11 +11,15 @@ import net.minecraft.entity.vehicle.VehicleEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static archives.tater.maglev.init.MaglevDataAttachments.HOVER_HEIGHT;
+
+@Debug(export = true)
 @SuppressWarnings("UnstableApiUsage")
 @Mixin(AbstractMinecartEntity.class)
 public abstract class AbstractMinecartEntityMixin extends VehicleEntity {
@@ -26,12 +31,18 @@ public abstract class AbstractMinecartEntityMixin extends VehicleEntity {
         super(entityType, world);
     }
 
-    @ModifyExpressionValue(
+    @WrapOperation(
             method = "getRailOrMinecartPos",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/AbstractMinecartEntity;getY()D")
+            at = @At(value = "NEW", target = "(III)Lnet/minecraft/util/math/BlockPos;")
     )
-    private double addHoverHeight(double original) {
-        return original - getAttachedOrElse(MaglevDataAttachments.HOVER_HEIGHT, 0);
+    private BlockPos addHoverHeight(int x, int y, int z, Operation<BlockPos> original) {
+        if (!hasAttached(HOVER_HEIGHT)) return original.call(x, y, z);
+        var hoverHeight = getAttachedOrElse(HOVER_HEIGHT, 0);
+        var altered = new BlockPos(x, y - hoverHeight + 1, z);
+        if (getWorld().getBlockState(altered).isIn(MaglevBlocks.MAGLEV_RAILS))
+            return altered;
+        removeAttached(HOVER_HEIGHT);
+        return original.call(x, y, z);
     }
 
     @ModifyExpressionValue(
@@ -39,20 +50,20 @@ public abstract class AbstractMinecartEntityMixin extends VehicleEntity {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isIn(Lnet/minecraft/registry/tag/TagKey;)Z")
     )
     private boolean setHoverHeight(boolean original, @Cancellable CallbackInfoReturnable<BlockPos> cir) {
-        if (original) return true;
+        if (original || hasAttached(HOVER_HEIGHT)) return true;
         // essentially adding an else clause
         var world = getWorld();
         var blockPos = getBlockPos().mutableCopy();
 
         for (int i = 1; i <= 15; i++) {
             blockPos.move(Direction.DOWN);
-            if (!world.getBlockState(blockPos).isIn(MaglevBlocks.MAGLEV_RAILS)) continue;
-            setAttached(MaglevDataAttachments.HOVER_HEIGHT, i);
-            cir.setReturnValue(blockPos.toImmutable());
-            return false;
+            if (world.getBlockState(blockPos).isIn(MaglevBlocks.MAGLEV_RAILS)) {
+                setAttached(HOVER_HEIGHT, i);
+                cir.setReturnValue(blockPos.toImmutable());
+                return false;
+            }
         }
 
-        removeAttached(MaglevDataAttachments.HOVER_HEIGHT);
         return false;
     }
 
@@ -61,7 +72,7 @@ public abstract class AbstractMinecartEntityMixin extends VehicleEntity {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/vehicle/MinecartController;getSpeedRetention()D")
     )
     private double frictionless(double original) {
-        return hasAttached(MaglevDataAttachments.HOVER_HEIGHT) ? 1 : original;
+        return hasAttached(HOVER_HEIGHT) ? 1 : original;
     }
 
 //    @Inject(
