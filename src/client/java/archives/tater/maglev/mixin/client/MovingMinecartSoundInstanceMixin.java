304cc3457523d5070a4cd9af0a29a7ea32d80b54
JosiahFu <josiahfu@gmail.com>
2025-08-15 23:30:59 -0700

    No sound for hovering minecarts

---
+++ src/client/java/archives/tater/maglev/mixin/client/MovingMinecartSoundInstanceMixin.java
@@ -0,0 +1,25 @@
+package archives.tater.maglev.mixin.client;
+
+import net.minecraft.client.sound.MovingMinecartSoundInstance;
+import net.minecraft.entity.vehicle.AbstractMinecartEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import static archives.tater.maglev.init.MaglevDataAttachments.HOVER_HEIGHT;
+
+@SuppressWarnings("UnstableApiUsage")
+@Mixin(MovingMinecartSoundInstance.class)
+public class MovingMinecartSoundInstanceMixin {
+    @Shadow @Final private AbstractMinecartEntity minecart;
+
+    @ModifyVariable(
+            method = "tick",
+            at = @At(value = "STORE", ordinal = 0)
+    )
+    private boolean cancelSound(boolean original) {
+        return original || minecart.hasAttached(HOVER_HEIGHT);
+    }
+}
