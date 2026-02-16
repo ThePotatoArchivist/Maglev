63c0fdeb874faac23ebf46305cebfa5919a33d99
ThePotatoArchivist <josiahfu@gmail.com>
2026-02-16 15:04:07 -0800

    Use tag in recipe instead of hardcoded sponge

--- src/main/java/archives/tater/maglev/emi/MaglevEmiPlugin.java
+++ src/main/java/archives/tater/maglev/emi/MaglevEmiPlugin.java
@@ -2,9 +2,9 @@ package archives.tater.maglev.emi;
 
 import archives.tater.maglev.Maglev;
 import archives.tater.maglev.init.MaglevBlocks;
+import archives.tater.maglev.init.MaglevItems;
 
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 
 import dev.emi.emi.api.EmiEntrypoint;
@@ -18,33 +18,33 @@ import java.util.stream.Stream;
 
 @EmiEntrypoint
 public class MaglevEmiPlugin implements EmiPlugin {
-    private static final EmiIngredient WET_SPONGE = EmiStack.of(Items.WET_SPONGE);
-
     private static String getPath(Block block) {
         return BuiltInRegistries.BLOCK.getKey(block).getPath();
     }
 
-    private static EmiWorldInteractionRecipe createOxidizingRecipe(Block before, Block after) {
+    private static EmiWorldInteractionRecipe createOxidizingRecipe(Block before, EmiIngredient oxidizers, Block after) {
         return EmiWorldInteractionRecipe.builder()
                 .id(Maglev.id("/manual_oxidation/" + getPath(after)))
                 .leftInput(EmiStack.of(before))
-                .rightInput(WET_SPONGE, true)
+                .rightInput(oxidizers, true)
                 .output(EmiStack.of(after))
                 .build();
     }
 
-    private static Stream<EmiWorldInteractionRecipe> createOxidizingRecipes(MaglevBlocks.WeatheringCopperBlocks blockSet) {
+    private static Stream<EmiWorldInteractionRecipe> createOxidizingRecipes(MaglevBlocks.WeatheringCopperBlocks blocks, EmiIngredient oxidizers) {
         return Stream.of(
-                createOxidizingRecipe(blockSet.unaffected(), blockSet.exposed()),
-                createOxidizingRecipe(blockSet.exposed(), blockSet.weathered()),
-                createOxidizingRecipe(blockSet.weathered(), blockSet.oxidized())
+                createOxidizingRecipe(blocks.unaffected(), oxidizers, blocks.exposed()),
+                createOxidizingRecipe(blocks.exposed(), oxidizers, blocks.weathered()),
+                createOxidizingRecipe(blocks.weathered(), oxidizers, blocks.oxidized())
         );
     }
 
     @Override
     public void register(EmiRegistry emiRegistry) {
+        var oxidizers = EmiIngredient.of(MaglevItems.OXIDIZERS);
+
         Stream.of(MaglevBlocks.MAGLEV_RAIL, MaglevBlocks.POWERED_MAGLEV_RAIL, MaglevBlocks.VARIABLE_MAGLEV_RAIL)
-                .flatMap(MaglevEmiPlugin::createOxidizingRecipes)
+                .flatMap(blockSet -> createOxidizingRecipes(blockSet, oxidizers))
                 .forEach(emiRegistry::addRecipe);
     }
 }
